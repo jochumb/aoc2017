e1a9586b6836e1f4094b48a7f54fbbcba65f45b8
Jochum Börger <jborger@xebia.com>
2017-12-01 16:48:52 +0100

    Remove duplicate code day 1

--- apps/day1/lib/day1.ex
+++ apps/day1/lib/day1.ex
@@ -1,16 +1,20 @@
 defmodule Day1 do
 
-  def part1(num_str) do
+  def part1(num_str), do: calculate(num_str, 1)
+
+  def part2(num_str), do: calculate(num_str, round(String.length(num_str)/2))
+
+  def calculate(num_str, rotation) do
     num_str
-      |> String.split("", trim: true)
-      |> Enum.map(&String.to_integer/1)
-      |> zip_with_rotated(1)
-      |> Enum.reduce(0, &add_if_equal/2)
+    |> String.split("", trim: true)
+    |> Enum.map(&String.to_integer/1)
+    |> zip_with_rotated(rotation)
+    |> Enum.reduce(0, &add_if_equal/2)
   end
 
   def zip_with_rotated(list, rotation) do
     rotate(list, rotation) 
-      |> Enum.zip(list)
+    |> Enum.zip(list)
   end
 
   def rotate(list, 0), do: list
@@ -18,13 +22,5 @@ defmodule Day1 do
 
   def add_if_equal({x, y}, acc) when x == y, do: acc + x
   def add_if_equal(_, acc), do: acc
-
-  def part2(num_str) do
-    num_str
-      |> String.split("", trim: true)
-      |> Enum.map(&String.to_integer/1)
-      |> zip_with_rotated(round(String.length(num_str)/2))
-      |> Enum.reduce(0, &add_if_equal/2)
-  end
 
 end
