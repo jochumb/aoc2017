cd84e60fd2c73edd834f70d1946a28c0b8b22929
Jochum Börger <jborger@xebia.com>
2017-12-04 19:42:07 +0100

    Little cleanup

--- apps/day04/lib/day04.ex
+++ apps/day04/lib/day04.ex
@@ -1,26 +1,16 @@
 defmodule Day04 do
   @moduledoc "Day 4: High-Entropy Passphrases"
 
-  def part1(str_list) do
-    str_list
-    |> Enum.filter(&is_valid_password/1)
-    |> Enum.count
+  def part1(str_list), do: solve_with_filter(str_list, &is_valid_password/1)
+
+  def part2(str_list), do: solve_with_filter(str_list, &is_valid_password_anagrams/1)
+
+  def solve_with_filter(str_list, filter) do
+    str_list |> Enum.filter(filter) |> Enum.count
   end
 
   def is_valid_password(str) do
-    str
-    |> String.split
-    |> has_no_duplicates
-  end
-
-  defp has_no_duplicates(list) do
-    Enum.count(list) == Enum.count(Enum.uniq(list))
-  end
-
-  def part2(str_list) do
-    str_list
-    |> Enum.filter(&is_valid_password_anagrams/1)
-    |> Enum.count
+    str |> String.split |> has_no_duplicates
   end
 
   def is_valid_password_anagrams(str) do
@@ -31,9 +21,10 @@ defmodule Day04 do
   end
 
   defp alphabetize_word(str) do
-    str
-    |> to_charlist
-    |> Enum.sort
-    |> to_string
+    str |> to_charlist |> Enum.sort
+  end
+
+  defp has_no_duplicates(list) do
+    Enum.count(list) == Enum.count(Enum.uniq(list))
   end
 end
