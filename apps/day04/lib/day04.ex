a0eb510bc36fb17ae541e80315797dd8b8d13210
Jochum Börger <jborger@xebia.com>
2017-12-04 22:27:25 +0100

    Minify day 4

--- apps/day04/lib/day04.ex
+++ apps/day04/lib/day04.ex
@@ -1,30 +1,12 @@
 defmodule Day04 do
   @moduledoc "Day 4: High-Entropy Passphrases"
-
-  def part1(str_list), do: solve_with_filter(str_list, &is_valid_password/1)
-
-  def part2(str_list), do: solve_with_filter(str_list, &is_valid_password_anagrams/1)
-
-  def solve_with_filter(str_list, filter) do
-    str_list |> Enum.filter(filter) |> Enum.count
-  end
-
-  def is_valid_password(str) do
-    str |> String.split |> has_no_duplicates
-  end
-
-  def is_valid_password_anagrams(str) do
-    str
-    |> String.split
-    |> Enum.map(&alphabetize_word/1)
-    |> has_no_duplicates
-  end
-
-  defp alphabetize_word(str) do
-    str |> to_charlist |> Enum.sort
-  end
-
-  defp has_no_duplicates(list) do
-    Enum.count(list) == Enum.count(Enum.uniq(list))
-  end
+  import Enum
+  import String, only: [split: 1]
+  def part1(i), do: i |> solve(&v1?/1)
+  def v1?(s), do: s |> split |> no_dup
+  def part2(i), do: i |> solve(&v2?/1)
+  def v2?(s), do: s |> split |> map(&ord/1) |> no_dup
+  defp solve(l, f), do: l |> filter(f) |> count
+  defp ord(s), do: s |> to_charlist |> sort
+  defp no_dup(l), do: l |> count == l |> uniq |> count
 end
--- apps/day04/test/day04_test.exs
+++ apps/day04/test/day04_test.exs
@@ -3,9 +3,9 @@ defmodule Day04Test do
   @moduledoc "Input for day 4"
 
     test "Test input part2" do
-      assert Day04.is_valid_password("aa bb cc dd ee") == true
-      assert Day04.is_valid_password("aa bb cc dd aa") == false
-      assert Day04.is_valid_password("aa bb cc dd aaa") == true
+      assert Day04.v1?("aa bb cc dd ee") == true
+      assert Day04.v1?("aa bb cc dd aa") == false
+      assert Day04.v1?("aa bb cc dd aaa") == true
     end
 
     test "Solution for part 1" do
@@ -16,11 +16,11 @@ defmodule Day04Test do
     end
 
     test "Test input part 2" do
-      assert Day04.is_valid_password_anagrams("abcde fghij") == true
-      assert Day04.is_valid_password_anagrams("abcde xyz ecdab") == false
-      assert Day04.is_valid_password_anagrams("a ab abc abd abf abj") == true
-      assert Day04.is_valid_password_anagrams("iiii oiii ooii oooi oooo") == true
-      assert Day04.is_valid_password_anagrams("oiii ioii iioi iiio") == false
+      assert Day04.v2?("abcde fghij") == true
+      assert Day04.v2?("abcde xyz ecdab") == false
+      assert Day04.v2?("a ab abc abd abf abj") == true
+      assert Day04.v2?("iiii oiii ooii oooi oooo") == true
+      assert Day04.v2?("oiii ioii iioi iiio") == false
     end
 
     test "Solution for part 2" do
