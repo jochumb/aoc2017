38247a3566dfd6ce325df22c899c5824ad49ee49
Jochum Börger <jborger@xebia.com>
2017-12-06 21:20:31 +0100

    Solved day 6

---
+++ apps/day06/lib/day06.ex
@@ -0,0 +1,82 @@
+defmodule Day06 do
+  @moduledoc "Day 6: Memory Reallocation"
+
+  def part1(input) do
+    {count, _} = input
+    |> to_indexed_int_map
+    |> solve
+    count
+  end
+
+  def part2(input) do
+    {_, state} = input
+    |> to_indexed_int_map
+    |> solve
+    state |> find_next(state)
+  end
+
+  defp to_indexed_int_map(input) do
+    input
+    |> String.split
+    |> Enum.map(&String.to_integer/1)
+    |> Enum.with_index
+    |> Enum.map(fn {k, v} -> {v, k} end)
+    |> Map.new
+  end
+
+  defp solve(map, seen \\ [], steps \\ 0) do
+    i = map |> min_index
+    new = map |> redistribute(i)
+    if new in seen do
+      {steps + 1, new}
+    else
+      solve(new, [new | seen], steps + 1)
+    end
+  end
+
+  defp find_next(map, state, steps \\ 0) do
+    i = map |> min_index
+    new = map |> redistribute(i)
+    if new == state do
+      steps + 1
+    else
+      find_next(new, state, steps + 1)
+    end
+  end
+
+  defp min_index(map) do
+    {index, _} = map
+    |> Map.keys
+    |> Enum.reduce({0, 0}, &(min_value(map, &1, &2)))
+    index
+  end
+
+  defp min_value(map, 0, _), do: {0, map[0]}
+  defp min_value(map, index, {_, val} = acc) do
+    case map[index] do
+      c when c > val -> {index, c}
+      _ -> acc
+    end
+  end
+
+  defp redistribute(map, index) do
+    %{^index => value} = map
+    map = %{map | index => 0}
+    redistribute_to_0(map, next_index(map, index), value)
+  end
+
+  defp redistribute_to_0(map, _, 0), do: map
+  defp redistribute_to_0(map, index, decr) do
+    %{^index => val} = map
+    map = %{map | index => val + 1}
+    map |> redistribute_to_0(next_index(map, index), decr - 1)
+  end
+
+  defp next_index(map, index) do
+    size = map |> Map.keys |> Enum.count
+    case index do
+      index when index + 1 < size -> index + 1
+      _ -> 0
+    end
+  end
+end
---
+++ apps/day06/test/day06_test.exs
@@ -0,0 +1,26 @@
+defmodule Day06Test do
+  use ExUnit.Case
+  @moduledoc "Input for day 6"
+
+  test "Test input part 1" do
+    assert Day06.part1("0	2	7	0") == 5
+  end
+
+  test "Solution for part 1" do
+    input_str = Utils.File.read_first_line_from_file("./input")
+    result = Day06.part1(input_str)
+    IO.puts "Day 6 part 1: #{result}"
+    assert result == 3_156
+  end
+
+  test "Test input part 2" do
+    assert Day06.part2("0	2	7	0") == 4
+  end
+
+  test "Solution for part 6" do
+    input_str = Utils.File.read_first_line_from_file("./input")
+    result = Day06.part2(input_str)
+    IO.puts "Day 6 part 2: #{result}"
+    assert result == 1_610
+  end
+end
